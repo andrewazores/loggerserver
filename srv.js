c16ab1613f273614cf6ca0fbad1d27df8ea023c7
Andrew Azores <aazores@redhat.com>
2023-09-20 10:30:38 -0400

    log request basic details

--- srv.js
+++ srv.js
@@ -10,7 +10,31 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.get('*', (req, res) => {
+const winston = require('winston');
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.printf((info) => `${info.timestamp} ${info.level.toUpperCase()}: ${info.message}`),
+  ),
+  transports: [
+    new winston.transports.Console(),
+  ],
+});
+
+const querystring = require('node:querystring');
+
+app.use('*', (req, _, next) => {
+  let str = `${req.method} ${req.path}`;
+  if (Object.keys(req.query).length) {
+    str += '?' + querystring.stringify(req.query);
+  }
+  str += '\n\t' + Object.entries(req.headers).map(v => `${v[0]}: ${v.slice(1)}`).join('\n\t');
+  logger.info(str);
+  next();
+});
+
+app.get('*', (_, res) => {
   res.status(200).send();
 });
 
