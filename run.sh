6d8ee9786852cf427b136bffcc4d296749889b40
Andrew Azores <aazores@redhat.com>
2023-09-20 11:14:39 -0400

    expose port

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/sh
 
+PORT=${PORT:-4000}
+
 podman run \
     --name loggerserver \
+    --publish "${PORT}:${PORT}" \
+    --env "PORT=${PORT}" \
     --rm -it quay.io/andrewazores/loggerserver:latest
